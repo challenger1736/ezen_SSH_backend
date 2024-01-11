ae1a3f05bb7c3d772544dc77b328efd0deab1675
challenger1736 <challenger1736@gmail.com>
2024-01-12 08:32:13 +0900

    Changes

--- src/day07/Step2.java
+++ src/day07/Step2.java
@@ -50,6 +50,7 @@ Step2 { // cs
         System.out.println("제작회사 = " + yourCar2.company);
         System.out.println(yourCar2.company==mycar2.company);
         // 리터럴이 문자열이기 때문에 문자열은 그 자체로 주소값과 비슷한 기능을 가지므로 문자열끼리 같다고 해도 true로 나온다.
+        // 생성자 안에서도 실험 해볼것.
         System.out.println("현재속도 = " + yourCar2.speed);
 
 
