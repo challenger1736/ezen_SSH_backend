dcacddd29f2e900ab679fd84a197a89b821c316a
challenger1736 <challenger1736@gmail.com>
2024-01-16 18:27:21 +0900

    Changes

---
+++ src/과제/과제5/Member.java
@@ -0,0 +1,22 @@
+package 과제.과제5;
+
+
+import java.util.Scanner;
+
+public class Member {
+    String 아이디;
+    String 비밀번호;
+    String 이름;
+    String 전화번호;
+    int 나이;
+
+    public Member(String 아이디, String 비밀번호, String 이름, String 전화번호, int 나이) {
+        this.아이디 = 아이디;
+        this.비밀번호 = 비밀번호;
+        this.이름 = 이름;
+        this.전화번호 = 전화번호;
+        this.나이 = 나이;
+    }
+
+
+}
