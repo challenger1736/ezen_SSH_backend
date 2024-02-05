7d2537ebbc02498c4df590052fdf033d936e2fbb
thzmvh <thzmvh2@naver.com>
2024-02-05 09:37:49 +0900

    commit
    Signed-off-by: thzmvh <thzmvh2@naver.com>

---
+++ src/DB/9fk_pk_cascade및_set_null_및_조인.sql
@@ -0,0 +1,126 @@
+
+/*
+
+	# DB : 데이터베이스 : 기계어 구성된 데이터들의 집합
+    
+			DBMS(RDBMS -> MYSQL, ORACLE )		<---- MYSQL WORKBENCH : DB연동 UI 소프트웨어 혹은  디비버 를 많이 씀
+            ^
+            |	SQL 명령어
+            |
+	  개발자(DBA) : 영어(SQL)
+
+
+	제약조건
+	1.not null
+    2.unique
+    3.default 값/함수()
+	4.(mysql) auto_increment *pk일 경우만 가능 but, insert into 자체에서 문제가 생겨서(DB문제) auto_increment 번호만 증가하는 경우가 있음( 그래서 수동으로 넣어주는게 나을수도 있음(자바로 계산해서 넣어주기))
+    5.primary key(pk필드명)
+    6.foreign key(fk필드명) references 참조할테이블(참조할pk필드명)
+		# 1.  제약조건에 이름 달기. ( 제약조건 이름을 부여해서 삭제할때 식별 - 자동 )
+			1-1. constraint 제약조건이름 primary key (pk필드명) 생략시 자동 이름 부여. // 우리가 직접 수정 삭제 할 것 같으면 constraint 하는 것임.
+            1-2. constraint 제약조건이름 foreign key(fk필드명) references 참조할테이블(참조할pk필드명)
+		# 2.  제약조건에 무결성 옵션 설정. 
+			* 무결성 : 데이터의 정확성, 일관성을 유지.
+			2-1. foreign키 명령어 제일 뒤 on delete restrict/cascade/noaction/set null.
+            2-2. foreign키 명령어 제일 뒤 on update restrict/cascade/noaction/set null.
+            	restict는 기본 값(pk-fk 데이터가 서로 참조일 경우 취소/실행불가) = noaction
+                cascade(pk-fk 데이터가 서로 참조일 경우 같이 삭제/업데이트)
+                set null(pk-fk 데이터가 서로 참조일 경우 foreign key를 null로 업데이트)
+                
+                
+	# 3. 조인 = 테이블 합치기 (다중 테이블을 검색, 불러오기 등 쓰는 용)
+		1. select *from 테이블명a, 테이블명b where 테이블명a.mno = 테이블명b.mno;
+		2. select *from 테이블명a natural join 테이블명b; // 단 fk pk 필드명 일치해야함
+    
+	# pk - fk
+    집합
+    1. 교집합 = pk와 fk 일치했을때,
+    2. 합집합 = pk와 fk 합치기.
+    3. 차집합 = pk와 fk 제외/뺀
+        
+*/
+drop database if exists test7;
+create database test7;
+use test7;
+
+#1. 회원 테이블
+drop table if exists member;
+create table member(
+	mno int auto_increment,
+    mid varchar(30),
+    mpw varchar(30),
+    
+    constraint member_mno_pk primary key(mno)
+);
+
+#2. 게시판 테이블
+drop table if exists board;
+create table board(
+	boardno int auto_increment,
+    boardtitle text,
+    boardcontent text,
+    
+    mno int,
+    
+    constraint board_bno_pk primary key (boardno),
+	constraint board_mno_fk foreign key (mno) references member(mno)
+    );
+
+#3. 샘플..
+insert into member(mid,mpw) values ('a','a');
+insert into member(mid,mpw) values ('b','b');
+insert into member(mid,mpw) values ('c','c');
+
+insert into board(boardtitle,boardcontent,mno) values ('자바','안녕하세요', 2);
+# 1번 회원 탈퇴
+# delete from member where mno = 1;
+
+# 1번 회원 회원번호 수정
+# update member set mno = 4 where mno =1 ; -- 업데이트도 restrict 안됨.
+select*from member;
+select*from board;
+
+insert into board(boardtitle,boardcontent,mno) values('자바2', '안녕하세요2', 1);
+
+ # 1번 게시물의 작성자 아이디를 알고 싶다.
+	# 1번 게시물의 작성자 mno 찾기
+    select mno from board where boardno = 1; -- 2
+    
+    # 찾은 mno를 가지고 회원의 아이디(mid) 찾기
+    select mid from member where mno = 2;	-- b
+    
+    ##################### 관계가 있는 테이블 간의 조인 해보기 ########################
+    
+    # 1. where 절을 이용한 합치기 [ 일반 조건 where 인지? 테이블을 합치기 위한 where 인지? 가독성 떨어짐. 실무 잘 안씀. ]
+    select * from member;
+    select * from board;
+    
+    select * from member, board; -- 중복이 생김 A테이블 레코드 1개마다 * B테이블 레코드 수 로 출력됨. (a레코드수 * b레코드수)
+    
+    #select * from member, board where mno = mno; 누구 mno인지 몰라서 안됨
+		# 1. 필드명 앞에 테이블을 붙인다.
+	select * from member,board where member.mno = board.mno order by boardno;
+		# 2. 테이블명에 별칭(검색시에만 사용되는 이름 - 원본 상관없음)
+	select * from member m,board b where m.mno = b.mno order by boardno;
+    #where 은 = 을 쓰고 join은 on = 을 쓴다;
+    
+    #2. JOIN 절 이용
+    # [ 교집합 -> 두 개 이상의 테이블 공통 데이터 찾기. ]
+		# 1. A natural join B
+    select *from member natural join board;
+		# 2. A join B on pk필드 = fk 필드
+    select *from member join board on member.mno = board.mno;
+		# 3. A join B using(관계필드명(같음))
+	select *from member join board using(mno); # 얘도 필드명이 같으면
+		# 4. A inner join B on pk필드 = fk필드
+	select *from member inner join board on member.mno = board.mno;
+		# 5. A right join B on pk 필드 = fk필드
+	select *from member right outer join board on member.mno = board.mno;
+		# 6. A left join B on pk 필드 = fk필드
+	select *from member left outer join board on member.mno = board.mno;
+    # 암기가 필요하다 ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+    
+    
+    
+    
---
+++ src/DB/javateamproject0203수정본.sql
@@ -0,0 +1,232 @@
+drop database if exists phoenix;
+create database phoenix;
+
+use phoenix;
+
+create table grade( -- 직위
+   gradeno int not null ,
+    gradename varchar(10),
+    
+    primary key(gradeno)
+);
+
+insert into grade values(0,'전체');
+insert into grade values(1,'사원');
+insert into grade values(2,'대리');
+insert into grade values(3,'팀장');
+insert into grade values(4,'부장');
+insert into grade values(5,'임원');
+
+
+create table part( -- 부서
+   partno int not null,
+    partname varchar(30) unique,
+    
+    primary key(partno)
+);
+
+insert into part values(1,'인사');
+insert into part values(2,'회계');
+insert into part values(3,'개발');
+
+create table employee( -- 사원
+   eno int auto_increment,
+    gradeno int not null default 0,
+   ename varchar(30) not null,
+    ephone varchar(15) not null unique,
+   eemail varchar(50) not null unique,
+    eid varchar(30) not null unique,
+    epw varchar(30) not null,
+    edate datetime default now(),
+    partno int,
+    
+    primary key(eno),
+    foreign key(gradeno) references grade(gradeno), 
+    foreign key(partno) references part(partno) on update cascade on delete set null
+);
+
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(1,'일사원','010-0000-0000','a@co.kr','a','a',1);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(2,'일대리','010-0000-0001','b@co.kr','b','b',1);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(3,'일팀장','010-0000-0002','c@co.kr','c','c',1);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(4,'일부장','010-0000-0003','d@co.kr','d','d',1);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(5,'일임원','010-0000-0004','e@co.kr','e','e',1);
+
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(1,'이사원','010-0000-0005','f@co.kr','f','f',2);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(2,'이대리','010-0000-0006','g@co.kr','g','g',2);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(3,'이팀장','010-0000-0007','h@co.kr','h','h',2);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(4,'이부장','010-0000-0008','i@co.kr','i','i',2);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(5,'이임원','010-0000-0009','j@co.kr','j','j',2);
+
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(1,'삼사원','010-0000-0010','k@co.kr','k','k',3);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(2,'삼대리','010-0000-0011','l@co.kr','l','l',3);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(3,'삼팀장','010-0000-0012','m@co.kr','m','m',3);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(4,'삼부장','010-0000-0013','n@co.kr','n','n',3);
+insert into employee(gradeno,ename,ephone,eemail,eid,epw,partno) values(5,'삼임원','010-0000-0014','o@co.kr','o','o',3);
+
+/*
+직급 샘플 데이터       마스터 : 임원 // 부장 팀장 대리 사원 
+*/
+
+/*
+부서 샘플 데이터       1회계 2인사 3개발 
+*/
+
+
+create table board(
+	boardno int auto_increment,
+    boardtitle varchar(30) not null,
+    boardcontent longtext not null,
+    eno int,
+    boarddate datetime default now(),
+   
+    primary key(boardno),
+    foreign key(eno) references employee(eno) on delete set null
+);
+
+insert into board(boardtitle,boardcontent,eno) values('1번 게시글','게시글 내용',1);
+insert into board(boardtitle,boardcontent,eno) values('2번 게시글','게시글 내용',2);
+insert into board(boardtitle,boardcontent,eno) values('3번 게시글','게시글 내용',3);
+insert into board(boardtitle,boardcontent,eno) values('4번 게시글','게시글 내용',4);
+insert into board(boardtitle,boardcontent,eno) values('5번 게시글','게시글 내용',5);
+insert into board(boardtitle,boardcontent,eno) values('6번 게시글','게시글 내용',6);
+insert into board(boardtitle,boardcontent,eno) values('7번 게시글','게시글 내용',7);
+insert into board(boardtitle,boardcontent,eno) values('8번 게시글','게시글 내용',8);
+insert into board(boardtitle,boardcontent,eno) values('9번 게시글','게시글 내용',9);
+insert into board(boardtitle,boardcontent,eno) values('10번 게시글','게시글 내용',10);
+insert into board(boardtitle,boardcontent,eno) values('11번 게시글','게시글 내용',11);
+insert into board(boardtitle,boardcontent,eno) values('12번 게시글','게시글 내용',12);
+insert into board(boardtitle,boardcontent,eno) values('13번 게시글','게시글 내용',13);
+insert into board(boardtitle,boardcontent,eno) values('14번 게시글','게시글 내용',14);
+insert into board(boardtitle,boardcontent,eno) values('15번 게시글','게시글 내용',15);
+
+create table boardpermit(
+   boardno int not null,
+    gradeno int default 0,
+    partno int default 0,
+    
+    
+    foreign key(boardno) references board(boardno) on delete cascade,
+    foreign key(gradeno) references grade(gradeno),
+	foreign key(partno) references part(partno) on update cascade on delete set null
+    
+);
+
+insert into boardpermit(boardno,gradeno,partno) values(1,1,1);
+insert into boardpermit(boardno,gradeno,partno) values(2,2,1);
+insert into boardpermit(boardno,gradeno,partno) values(3,3,1);
+insert into boardpermit(boardno,gradeno,partno) values(4,4,1);
+insert into boardpermit(boardno,gradeno,partno) values(5,5,1);
+
+insert into boardpermit(boardno,gradeno,partno) values(6,1,2);
+insert into boardpermit(boardno,gradeno,partno) values(7,2,2);
+insert into boardpermit(boardno,gradeno,partno) values(8,3,2);
+insert into boardpermit(boardno,gradeno,partno) values(9,4,2);
+insert into boardpermit(boardno,gradeno,partno) values(10,5,2);
+
+insert into boardpermit(boardno,gradeno,partno) values(11,1,3);
+insert into boardpermit(boardno,gradeno,partno) values(12,2,3);
+insert into boardpermit(boardno,gradeno,partno) values(13,3,3);
+insert into boardpermit(boardno,gradeno,partno) values(14,4,3);
+insert into boardpermit(boardno,gradeno,partno) values(15,5,3);
+
+-- gradno 별 3개, partno 별 5개
+
+create table reply(
+	eno int,
+    boardno int not null,
+    replyno int auto_increment,
+    replycontent text not null,
+    replydate datetime default now(),
+    
+    primary key (replyno),
+    foreign key (eno) references employee(eno) on delete set null,
+    foreign key (boardno) references board(boardno) on delete cascade
+);
+
+insert into reply(eno,boardno,replycontent) values(2,1,"댓글내용1");
+insert into reply(eno,boardno,replycontent) values(3,1,"댓글내용2");
+insert into reply(eno,boardno,replycontent) values(2,2,"댓글내용3");
+
+create table report(
+	eno int,
+	reportno int auto_increment,
+	reporttitle varchar(50) not null,
+    reportcontent longtext not null,
+    reportdate datetime default now(),
+    
+    primary key(reportno),
+    foreign key(eno) references employee(eno) on delete set null
+);
+
+create table reportlog(
+	reportno int not null,
+    eno int,
+    confirm bool default false,
+    
+    foreign key(reportno) references report(reportno) on delete cascade,
+    foreign key(eno) references employee(eno) on delete set null
+);
+
+select * from employee;
+select * from board;
+select * from boardpermit;
+select * from reply;
+
+
+select * from report;
+select * from reportlog;
+-- 심재훈
+SELECT  report.reportno ,COUNT(*)FROM report JOIN reportlog ON report.reportno = reportlog.reportno GROUP BY report.reportno;
+SELECT  report.reportno ,reportlog.confirm FROM report JOIN reportlog ON report.reportno = reportlog.reportno where report.eno = 1 GROUP BY report.reportno, reportlog.confirm;
+SELECT report.*,reportlog.eno FROM report JOIN reportlog ON report.reportno = reportlog.reportno WHERE reportlog.eno = 1;
+SELECT report.*,reportlog.confirm FROM report JOIN reportlog ON report.reportno = reportlog.reportno WHERE reportlog.eno = 4;
+
+SELECT  report.* ,reportlog.confirm FROM report JOIN reportlog ON report.reportno = reportlog.reportno;
+
+SELECT  report.* ,reportlog.confirm FROM report JOIN reportlog ON report.reportno = reportlog.reportno where reportlog.eno = 2;
+
+
+
+SElect report.reportno,Count(*) as count FROM report JOIN reportlog ON report.reportno = reportlog.reportno group by report.reportno;
+SELECT report.reportno,Count(*) FROM report JOIN reportlog ON report.reportno = reportlog.reportno WHERE reportlog.confirm =true group by report.reportno, reportlog.confirm;
+
+select * from report where reportno = 5;
+
+select * from report;
+select * from reportlog;
+
+insert report(eno,reporttitle,reportcontent) values(1,"안녕","자바");
+select * from reportlog;
+
+SELECT report.*, r1.log_count, r2.log_count_confirm
+FROM report 
+LEFT JOIN (SELECT report.reportno, COUNT(*) as log_count
+			FROM report JOIN reportlog ON report.reportno = reportlog.reportno GROUP BY report.reportno) r1 ON report.reportno = r1.reportno
+LEFT JOIN ( SELECT report.reportno, COUNT(*) AS log_count_confirm FROM report JOIN reportlog ON report.reportno = reportlog.reportno WHERE reportlog.confirm = true GROUP BY report.reportno, reportlog.confirm) r2 
+ON report.reportno = r2.reportno;
+
+
+DELIMITER //
+
+CREATE EVENT IF NOT EXISTS update_report_confirm
+ON SCHEDULE EVERY 1 DAY
+DO
+BEGIN
+  UPDATE reportlog
+  SET confirm = false
+  WHERE reportno IN (
+    SELECT report.reportno
+    FROM report
+    LEFT JOIN (
+      SELECT report.reportno, COUNT(*) FROM report JOIN reportlog ON report.reportno = reportlog.reportno GROUP BY report.reportno
+    ) r1 ON report.reportno = r1.reportno
+    LEFT JOIN (
+      SELECT report.reportno, COUNT(*) FROM report JOIN reportlog ON report.reportno = reportlog.reportno WHERE reportlog.confirm = true GROUP BY report.reportno, reportlog.confirm
+    ) r2 ON report.reportno = r2.reportno
+    WHERE report.reportdate < NOW() - INTERVAL 7 DAY AND COALESCE(r1.count, 0) != COALESCE(r2.count, 0)
+  );
+END//
+
+DELIMITER ;
+
+show events;
